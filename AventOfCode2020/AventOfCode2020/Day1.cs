09fbca77b3c0d38f28633e4dbdd482817b39db27
D40\dhm441 <patrick.verheij@engie.com>
2020-12-01 09:39:56 +0100

    initial version

---
+++ AventOfCode2020/AventOfCode2020/Day1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AventOfCode2020
+{
+    class Day1
+    {
+        public static List<int> GetnumbersAssignment1()
+        {
+            List<int> result = new List<int>();
+            result.Add(32);
+
+
+            return result;
+
+
+        }
+
+
+
+
+        public static int CalculateNumbers(List<int> numbers)
+        {
+            foreach (var number1 in numbers)
+            {
+                foreach (var number2 in numbers)
+                {
+                    if (number1 + number2 == 2020)
+                    {
+                        Console.WriteLine($"Nummer1 = {number1}");
+                        Console.WriteLine($"Nummer2 = {number2}");
+                        var result = number1 * number2;
+                        Console.WriteLine($"Resultaat = {result}");
+                        return result;
+                    }
+                    return 0;
+                }
+                return 0;
+            }
+            return 0;
+        }
+
+
+    }
+}
+
---
+++ AventOfCode2020/AventOfCode2020/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AventOfCode2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = Day1.GetnumbersAssignment1();
+            var result = Day1.CalculateNumbers(list);
+            Console.WriteLine($"Totaal resultaat = {result}");
+        }
+    }
+}
